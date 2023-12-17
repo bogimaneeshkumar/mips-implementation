cce7cd2de4ce2ec54e6688e465b757b9a7984cf6
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-17 08:59:50 +0530

    Update regm.v

--- regm.v
+++ regm.v
@@ -1,39 +1,41 @@
 /*
- * regm - register memory
+ * register memory bank
  *
- * A 32-bit register memory.  Two registers can be read at once. The
- * variables `read1` and `read2` specify which registers to read.  The
- * output is placed in `data1` and `data2` and at same time one register can
- * be write with wrdata to register specified by wrreg
+ * A 32-bit register memory bank.  with two read ports and one write port
  *
+ *The register at address 0, the value read is always zero.
  */
 
-`ifndef _regm
-`define _regm
+module regm (
+    input wire clk,
+    input wire [4:0] read1, read2,
+    output wire [31:0] data1, data2,
+    input wire regwrite,
+    input wire [4:0] wrreg,
+    input wire [31:0] wrdata
+);
 
-`ifndef DEBUG_CPU_REG
-`define DEBUG_CPU_REG 0
-`endif
+    reg [31:0] mem [0:31]; // 32-bit memory with 32 entries
 
-module regm( write, clk, wrreg, read1, read2, data1, data2, wrdata);
+    always @(*) begin
+        if (read1 == 5'd0)
+            data1 = 32'd0;
+	    else if (read1 == wrreg && regwrite)   //If the register being read is the same as that being written,value being written will be available immediately without a one cycle delay.
+            data1 = wrdata;
+        else
+            data1 = mem[read1];
 
-		input   write, clk;
-                input   [4:0] wrreg, read1, read2;
-                output  [31:0]  data1, data2;
-                input   [31:0]  wrdata;
+        if (read2 == 5'd0)
+            data2 = 32'd0;
+	    else if (read2 == wrreg && regwrite)  //If the register being read is the same as that being written, the value being written will be available immediately without a one cycle delay.
+            data2 = wrdata;
+        else
+            data2 = mem[read2];
+    end
 
-	reg [31:0] mem [0:31];  // 32-bit memory regfile 
+    always @(posedge clk) begin
+        if (regwrite && wrreg != 5'd0)
+            mem[wrreg] <= wrdata; // write a non-zero register
+    end
 
-
-
-	assign data1 = mem[read1];
-	assign data2 = mem[read2];
-
-	always @(posedge clk) begin
-		if (write ) begin
-			mem[wrreg] <= wrdata;
-		end
-	end
 endmodule
-
-`endif
