f3539cac96c16529c146b03393226d65ffa13e22
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-17 10:14:27 +0530

    Update and rename regm.v to register_bank.v

--- register_bank.v
+++ register_bank.v
@@ -9,7 +9,7 @@
 module regm (
     input wire clk,
     input wire [4:0] read1, read2,
-    output wire [31:0] data1, data2,
+    output reg [31:0] data1, data2,
     input wire regwrite,
     input wire [4:0] wrreg,
     input wire [31:0] wrdata
