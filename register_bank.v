7c87339b5a9ae9f849d9f2bf8399b8f7f4167fae
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-17 10:14:40 +0530

    Update register_bank.v

--- register_bank.v
+++ register_bank.v
@@ -10,7 +10,7 @@ module regm (
     input wire clk,
     input wire [4:0] read1, read2,
     output reg [31:0] data1, data2,
-    input wire regwrite,
+    input wire regwrite ,
     input wire [4:0] wrreg,
     input wire [31:0] wrdata
 );
