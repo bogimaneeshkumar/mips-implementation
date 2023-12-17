02bf9e30990f69b4c934a4397a746d24b88e065d
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-17 10:25:23 +0530

    Update alu_control.v

--- alu_control.v
+++ alu_control.v
@@ -1,5 +1,14 @@
-`ifndef _alu_control
-`define _alu_control
+/*   in the instruction immediate or register type the 1st 
+*    6 bits from MSB correcponds to the opp code the func 
+*    means the oppcode
+*
+*    Here we filtering 4 bits out of those  bit oppcode to 
+*    give oppcode to the alu 
+*
+*     As we are implementing less no of instructions by our ALU 4 bit is sufficient for oppcode
+*     remaining 2 bits can be used to control the mux near the Alu input
+*/
+
 
 module alu_control(
 		input wire [5:0] funct,
@@ -12,7 +21,7 @@ module alu_control(
 			4'd5:  aluctl = 4'd1;	/* or */
 			4'd6:  aluctl = 4'd13;	/* xor */
 			4'd7:  aluctl = 4'd12;	/* nor */
-			4'd10: aluctl = 4'd7;	/* slt */
+			4'd10: aluctl = 4'd7;	/* nand */
 			default: aluctl = 4'd0;
 		endcase
 	end
@@ -20,4 +29,3 @@ module alu_control(
 
 endmodule
 
-`endif
