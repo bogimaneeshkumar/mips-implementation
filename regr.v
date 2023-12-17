d71142b174563155b09db47636e68b5ea3004dfe
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-17 08:39:09 +0530

    Update regr.v

--- regr.v
+++ regr.v
@@ -4,26 +4,23 @@
  * regr - register of data that can be held or cleared
  */
 
-`ifndef _regr
-`define _regr
+module regr #(
+    parameter N = 1
+) (
+    input clk,
+    input clear,
+    input hold,
+    input wire [N-1:0] in,
+    output reg [N-1:0] out
+);
 
-module regr (
-	input clk,
-	input clear,
-	input hold,
-	input wire [N-1:0] in,
-	output reg [N-1:0] out);
+    always_ff @(posedge clk) begin
+        if (clear)
+            out <= {N{1'b0}};
+        else if (hold)
+            out <= out;
+        else
+            out <= in;
+    end
 
-	parameter N = 1;
-
-	always @(posedge clk) begin
-		if (clear)
-			out <= {N{1'b0}};
-		else if (hold)
-			out <= out;
-		else
-			out <= in;
-	end
 endmodule
-
-`endif
