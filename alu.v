795debaa49ff486e304b85f7afd3a7b4f38a1ec8
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2023-12-18 11:34:05 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -3,7 +3,7 @@
 */
 
 module alu(
-		input		[3:0]	ctl,
+	input		[5:0]	opp_code,
 		input		[31:0]	a, b,
 		output reg	[31:0]	out,
 		output	reg	zero);
@@ -15,18 +15,17 @@ module alu(
 
 	assign sub_ab = a - b;
 	assign add_ab = a + b;
-	/*rc_adder #(.N(32)) add0(.a(a), .b(b), .s(add_ab));*/
 
 
 	always @(*) begin
-		case (ctl)
-			4'd2:  out <= add_ab;				/* add */
-			4'd0:  out <= a & b;				/* and */
-			4'd12: out <= ~(a | b);				/* nor */
-			4'd1:  out <= a | b;				/* or */
-			4'd7:  out <= ~(a & b);	       			 /* nand */ 
-			4'd6:  out <= sub_ab;				/* sub */
-			4'd13: out <= a ^ b;				/* xor */
+		case (opp_code)
+			6'd2:  out <= add_ab;				/* add */
+			6'd0:  out <= a & b;				/* and */
+			6'd12: out <= ~(a | b);				/* nor */
+			6'd1:  out <= a | b;				/* or */
+			6'd7:  out <= ~(a & b);	       			 /* nand */ 
+			6'd6:  out <= sub_ab;				/* sub */
+			6'd13: out <= a ^ b;				/* xor */
 			default: out <= 0;
 		endcase
 	end
