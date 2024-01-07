522b0258e5487f1e76fc681b699a24cda1a8ad61
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2024-01-07 19:46:37 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -2,8 +2,10 @@
 *  ALU
 */
 
+
+
 module alu(
-	input		[5:0]	opp_code,
+	input		[3:0]	alu_op,
 		input		[31:0]	a, b,
 		output reg	[31:0]	out,
 		output	reg	zero);
@@ -18,17 +20,16 @@ module alu(
 
 
 	always @(*) begin
-		case (opp_code)
-			6'd2:  out <= add_ab;				/* add */
-			6'd0:  out <= a & b;				/* and */
-			6'd12: out <= ~(a | b);				/* nor */
-			6'd1:  out <= a | b;				/* or */
-			6'd7:  out <= ~(a & b);	       			 /* nand */ 
-			6'd6:  out <= sub_ab;				/* sub */
-			6'd13: out <= a ^ b;				/* xor */
+		case (alu_op)
+			4'd2:  out <= add_ab;				/* add */
+			4'd0:  out <= a & b;				/* and */
+			4'd12: out <= ~(a | b);				/* nor */
+			4'd1:  out <= a | b;				/* or */
+			4'd7:  out <= ~(a & b);	       			 /* nand */ 
+			4'd6:  out <= sub_ab;				/* sub */
+			4'd13: out <= a ^ b;				/* xor */
 			default: out <= 0;
 		endcase
 	end
 
 endmodule
-
