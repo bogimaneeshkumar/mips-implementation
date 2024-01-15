d17810f21bfc8e81ae64b57d3366b666988a7f9f
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2024-01-15 17:13:16 +0530

    Update cpu_tb.v

--- test/cpu_tb.v
+++ test/cpu_tb.v
@@ -5,23 +5,7 @@
  *
  * DESCRIPTION
  *
- * This generic cpu test bench can be used to run a program, which is in
- * ASCII hex format, and output the results.
- *
- * Configuration is done by setting preprocessor defines at compile
- * time.  The result is an executable for that specific test.
- *
- *   iverilog -DIM_DATA_FILE="\"t0001-no_hazard.hex\"" \
- *            -DNUM_IM_DATA=`wc -l t0001-no_hazard.hex | awk {'print $$1'}` \
- *            -DDUMP_FILE="\"t0001-no_hazard.vcd\"" \
- *            -I../ -g2005 \
- *            -o t0001-no_hazard \
- *            cpu_tb.v
- *
- * Then it can be run in the usual manner.  $monitor variables will be
- * output to STDOUT and a .vcd for use with Gtkwave will be output to
- * 'DUMP_FILE'.
- *
+ * 
  *   ./t0001-no_hazard > t0001-no_hazard.out
  */
 
