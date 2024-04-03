32e759943020b3c087821ba86eeb563c2db0ad92
bogimaneeshkumar <98801332+bogimaneeshkumar@users.noreply.github.com>
2024-04-03 17:54:23 +0530

    Update instruction_memory.v

--- instruction_memory.v
+++ instruction_memory.v
@@ -4,17 +4,12 @@
  */
 
 
-module im(
-		input wire	clk,
+module im #( 	parameter NMEM = 32, IM_DATA=128;  )   (	input wire	clk,
 		input wire 	[31:0] 	addr,
 		output wire [31:0] 	data);
 
-	parameter NMEM = 128;   
+	reg [NMEM-1:0] mem [0:IM_DATA-1];  // 32-bit memory with 128 entries
 
-	reg [31:0] mem [0:127];  // 32-bit memory with 128 entries
-
-	//actually 2^32 is very big size to systhesis not supported in vivado so currentlt i am decoding(ectracting) only 1st 7 bits of addr 
-
-	assign data = mem[addr[6:0]];  //addr is the PC program counter
+	assign data = mem[addr];  //addr is the PC program counter
 	
 endmodule
